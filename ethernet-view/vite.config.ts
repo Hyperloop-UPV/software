a3fd49bdee16841baef77ae3e9432eef8d5c2a11
Maxim <74974283+maximka76667@users.noreply.github.com>
2025-11-11 09:22:53 +0100

    feat: basic electron setup

--- ethernet-view/vite.config.ts
+++ ethernet-view/vite.config.ts
@@ -7,20 +7,21 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    build: {
-        sourcemap: true,
-        outDir: "static",
-        minify: false,
+  base: "./", // Add this line for relative paths
+  build: {
+    sourcemap: true,
+    outDir: "static",
+    minify: false,
+  },
+  plugins: [react(), tsconfigPaths(), svgr()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/tests/setup.ts",
+  },
+  resolve: {
+    alias: {
+      common: path.resolve(__dirname, "../common-front"),
     },
-    plugins: [react(), tsconfigPaths(), svgr()],
-    test: {
-        globals: true,
-        environment: "jsdom",
-        setupFiles: "./src/tests/setup.ts",
-    },
-    resolve: {
-        alias: {
-          common: path.resolve(__dirname, '../common-front'),
-        },
-      }, 
+  },
 });
