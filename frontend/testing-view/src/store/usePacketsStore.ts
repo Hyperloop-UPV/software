3483a049372346fc4d2b51ce8aa4e83d7c8bbfb2
Maxim <74974283+maximka76667@users.noreply.github.com>
2025-12-14 12:37:51 +0100

    refact: add default filter in create filterable store

--- frontend/testing-view/src/store/usePacketsStore.ts
+++ frontend/testing-view/src/store/usePacketsStore.ts
@@ -3,8 +3,10 @@ import { MOCK_PACKETS } from "../mocks/packets";
 import type { PacketsBoardName } from "../types/TabFilter";
 import type { Packet } from "../types/Packet";
 import { PACKET_BOARD_NAMES } from "../constants/boards";
+import { createFullFilter } from "../lib/utils";
 
 export const usePacketsStore = createFilterableStore<PacketsBoardName, Packet>({
   categories: PACKET_BOARD_NAMES,
-  getMockData: () => MOCK_PACKETS,
+  dataSource: MOCK_PACKETS,
+  defaultFilter: createFullFilter(PACKET_BOARD_NAMES, MOCK_PACKETS),
 });
