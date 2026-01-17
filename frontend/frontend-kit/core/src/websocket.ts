52c276903079b84f7bd74c929fc0e9b97acbadf1
Maxim <74974283+maximka76667@users.noreply.github.com>
2026-01-17 10:18:05 +0100

    test: remote url

--- frontend/frontend-kit/core/src/websocket.ts
+++ frontend/frontend-kit/core/src/websocket.ts
@@ -68,7 +68,7 @@ class SocketService {
     return this.messages$.pipe(
       filter((msg) => msg.topic === topic),
       map((msg) => msg.payload),
-      throttleTime(100, asyncScheduler),
+      throttleTime(80, asyncScheduler),
     );
   }
 
