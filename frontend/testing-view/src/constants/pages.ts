f79d21d87e6dc29e3a1e73fd674ec0611870519c
Maxim <74974283+maximka76667@users.noreply.github.com>
2026-01-20 18:24:59 +0100

    feat: remove camera page

--- frontend/testing-view/src/constants/pages.ts
+++ frontend/testing-view/src/constants/pages.ts
@@ -1,9 +1,8 @@
-import { Camera, FlaskConical, ScrollText } from "@workspace/ui/icons";
+import { FlaskConical, ScrollText } from "@workspace/ui/icons";
 
 export const PAGES = {
   "/": { title: "Testing", icon: FlaskConical },
   "/logs": { title: "Logs", icon: ScrollText },
-  "/camera-view": { title: "Camera View", icon: Camera },
 };
 
 export const PAGES_ARRAY = Object.entries(PAGES).map(
