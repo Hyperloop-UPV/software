757f548ff945a182a398ce6e8e083c6378748582
Vasyl <vasylks@outlook.es>
2026-02-12 09:44:18 +0100

    chore(packet-sender): clean up main.go for readability and robustness

--- packet-sender/main.go
+++ packet-sender/main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"bytes"
-	"encoding/binary"
 	"fmt"
-	"io"
 	"log"
-	"math"
 	"net"
-	"os"
 	"path"
 	"path/filepath"
+	"strings"
 
 	adj_module "github.com/HyperloopUPV-H8/h9-backend/pkg/adj"
 )
@@ -22,7 +18,6 @@ type boardConn struct {
 }
 
 func main() {
-
 	adj := getADJ()
 
 	backend_address := adj.Info.Addresses["BACKEND"]
@@ -31,7 +26,7 @@ func main() {
 	// Create a listener so the packets don't get lost (the backend will sniff them)
 	listener, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.ParseIP(backend_address), Port: int(backend_port)})
 	if err != nil {
-		log.Fatalf("Error creando listener UDP: %v", err)
+		log.Fatalf("failed to create UDP listener: %v", err)
 	}
 	defer listener.Close()
 
@@ -48,11 +43,16 @@ func main() {
 		getBoardPackets(&conns[i])
 	}
 
+	fmt.Print("Do you want to send random packets (1) or manual packets (2)? ")
 	var input string
-	fmt.Print("Quieres lanzar paquetes aleatorios (1) o manualmente (2): ")
 	_, err = fmt.Scan(&input)
 	if err != nil {
-		log.Fatal("Invalid input", err)
+		log.Fatalf("failed to read input: %v", err)
+	}
+
+	input = strings.TrimSpace(input)
+	if input != "1" && input != "2" {
+		log.Fatal("invalid input: use 1 or 2")
 	}
 
 	if input == "1" {
@@ -112,7 +112,6 @@ func getADJ() adj_module.ADJ {
 }
 
 func getBoardPackets(boardconn *boardConn) {
-
 	packets := make([]adj_module.Packet, 0)
 
 	for _, packet := range boardconn.board.Packets {
@@ -130,118 +129,4 @@ func getBoardPackets(boardconn *boardConn) {
 	}
 
 	boardconn.packets = packets
-
-}
-
-func mapNumberToRange(number float64, numberRange []*float64, numberType string) float64 {
-	if len(numberRange) == 0 {
-		return number * getTypeMaxValue(numberType)
-	} else {
-		return (number * (*numberRange[1] - *numberRange[0])) + *numberRange[0]
-	}
-}
-
-func getTypeMaxValue(numberType string) float64 {
-	switch numberType {
-	case "uint8":
-		return math.MaxUint8
-	case "uint16":
-		return math.MaxUint16
-	case "uint32":
-		return math.MaxUint32
-	case "uint64":
-		return math.MaxUint64
-	case "int8":
-		return math.MaxInt8
-	case "int16":
-		return math.MaxInt16
-	case "int32":
-		return math.MaxInt32
-	case "int64":
-		return math.MaxInt64
-	case "float32":
-		return math.MaxFloat32
-	case "float64":
-		return math.MaxFloat64
-	case "bool":
-		return math.MaxUint8
-	default:
-		return math.MaxUint8
-	}
-}
-
-func writeNumberAsBytes(number float64, numberType string, buff *bytes.Buffer) {
-	switch numberType {
-	case "uint8":
-		binary.Write(buff, binary.LittleEndian, uint8(number))
-	case "uint16":
-		binary.Write(buff, binary.LittleEndian, uint16(number))
-
-	case "uint32":
-		binary.Write(buff, binary.LittleEndian, uint32(number))
-
-	case "uint64":
-		binary.Write(buff, binary.LittleEndian, uint64(number))
-
-	case "int8":
-		binary.Write(buff, binary.LittleEndian, int8(number))
-
-	case "int16":
-		binary.Write(buff, binary.LittleEndian, int16(number))
-
-	case "int32":
-		binary.Write(buff, binary.LittleEndian, int32(number))
-
-	case "int64":
-		binary.Write(buff, binary.LittleEndian, int64(number))
-
-	case "float32":
-		binary.Write(buff, binary.LittleEndian, float32(number))
-
-	case "float64":
-		binary.Write(buff, binary.LittleEndian, number)
-
-	case "bool":
-		binary.Write(buff, binary.LittleEndian, uint8(number))
-
-	default:
-		binary.Write(buff, binary.LittleEndian, uint8(number))
-	}
-}
-
-// Copies a directory recursively from src to dst
-func CopyDir(src string, dst string) error {
-	return filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		relPath, err := filepath.Rel(src, path)
-		if err != nil {
-			return err
-		}
-		targetPath := filepath.Join(dst, relPath)
-		if info.IsDir() {
-			return os.MkdirAll(targetPath, info.Mode())
-		}
-		// Copy file
-		return copyFile(path, targetPath, info.Mode())
-	})
-}
-
-func copyFile(src, dst string, perm os.FileMode) error {
-	in, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer in.Close()
-	out, err := os.Create(dst)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-	_, err = io.Copy(out, in)
-	if err != nil {
-		return err
-	}
-	return out.Chmod(perm)
 }
