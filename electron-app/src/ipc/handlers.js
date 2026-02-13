d88b437232eececb0d8146bf4d519b58355946a0
Maxim <74974283+maximka76667@users.noreply.github.com>
2026-02-13 19:53:17 +0100

    fix: background throttling and first packet rerender

--- electron-app/src/ipc/handlers.js
+++ electron-app/src/ipc/handlers.js
@@ -7,19 +7,19 @@
  * - Folder selection dialogs
  */
 
-import { ipcMain, dialog } from "electron";
+import { dialog, ipcMain } from "electron";
 import {
+  importConfig,
   readConfig,
   writeConfig,
-  importConfig,
 } from "../config/configInstance.js";
-import {
-  loadView,
-  getCurrentView,
-  getMainWindow,
-} from "../windows/mainWindow.js";
 import { restartBackend } from "../processes/backend.js";
 import { logger } from "../utils/logger.js";
+import {
+  getCurrentView,
+  getMainWindow,
+  loadView,
+} from "../windows/mainWindow.js";
 
 /**
  * Initializes all IPC handlers for communication between main and renderer processes.
