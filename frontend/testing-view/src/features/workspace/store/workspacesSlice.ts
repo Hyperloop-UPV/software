7ccfe4ba9907591c5ab0f1f09a1abe6327754ca2
Maxim <74974283+maximka76667@users.noreply.github.com>
2026-02-20 15:56:16 +0100

    check

--- frontend/testing-view/src/features/workspace/store/workspacesSlice.ts
+++ frontend/testing-view/src/features/workspace/store/workspacesSlice.ts
@@ -68,6 +68,7 @@ export const createWorkspacesSlice: StateCreator<
           logs: createFullFilter(telemetry, get().boards),
         },
       };
+      // test
 
       // Initialize expanded items for the new workspace
       const newExpandedItems = {
