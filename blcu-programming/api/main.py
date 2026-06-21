f375852e541c986dfca36c1e62825b2a0d07908d
Javier Ribal del Rio <javierribal@gmail.com>
2026-06-21 11:06:07 +0200

    fix(BLCU-programming): add env data

--- blcu-programming/api/main.py
+++ blcu-programming/api/main.py
@@ -4,9 +4,11 @@
 # - UDP server to receive BLCU status (ADJ)
 # - TCP to send the order to the BLCU (ADJ)
 # Vasyl Kilimenko, Lola Castillo & Javier Ribal del Río 11
+import os
 import uvicorn
 
 from api.config import load
+from api.http_server import app
 import api.board_pinger as board_pinger
 import api.tcp_client as tcp_client
 import api.udp_server as udp_server
@@ -15,15 +17,19 @@ import api.udp_server as udp_server
 def main() -> None:
     cfg = load()["api"]
 
+    host = os.environ.get("BLCU_API_HOST", cfg["host"])
+    port = int(os.environ.get("BLCU_API_PORT", cfg["port"]))
+
     udp_server.start()
     board_pinger.start()
     tcp_client.start()
 
     uvicorn.run(
-        "api.http_server:app",
-        host=cfg["host"],
-        port=cfg["port"],
+        app,
+        host=host,
+        port=port,
         log_level=cfg["log_level"],
+        access_log=False,
     )
 
 
